2151bd58581a7a9a353d605606d06a1b962b3dff
Brajananda Mahata <brajanandamahato280603@gmail.com>
2024-01-03 18:39:17 +0530

    first commit

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,30 @@
+<script>
+	import '../app.postcss';
+	import { AppShell, AppBar, LightSwitch } from '@skeletonlabs/skeleton';
+</script>
+
+<!-- App Shell -->
+<AppShell>
+	<svelte:fragment slot="header">
+		<!-- App Bar -->
+		<AppBar>
+			<svelte:fragment slot="lead">
+				<a
+				class="btn m-0 p-0 variant-ghost-surface btn-icon me-2"
+				href="https://github.com/Brajo280603/ImageCompressorNODE"
+				target="_blank"
+			>
+				<img src="/github.svg" alt="">
+			</a>
+				<strong class="text-xl uppercase">image Compressor</strong>
+			</svelte:fragment>
+			<svelte:fragment slot="trail">
+	
+
+				<LightSwitch />
+			</svelte:fragment>
+		</AppBar>
+	</svelte:fragment>
+	<!-- Page Route Content -->
+	<slot />
+</AppShell>
---
+++ src/routes/+page.svelte
@@ -0,0 +1,170 @@
+<script>
+  import imageCompression from 'browser-image-compression'
+	import { ProgressRadial } from '@skeletonlabs/skeleton';
+
+  let files = [];
+  let imageSize;
+  let imageHeight;
+  let imageWidth;
+  let optionSelect = "Size"; // Set a default value
+	let currentProcessingIndex = 0;
+	
+
+  async function compress_image() {
+    // Implement your image compression logic here
+		currentProcessingIndex = 0;
+
+		let allFiles = []
+
+		let allBlobs = []
+
+
+		for(const file of files){
+			console.log(file)
+			// imageCompressorFunction(file)
+			allFiles.push(file)
+		}
+
+
+		while (currentProcessingIndex < allFiles.length) {
+			// Process 10 files at a time
+			for (let i = 0; i < 10 && currentProcessingIndex < allFiles.length; i++) {
+				let Fileblob = await imageCompressorFunction(allFiles[currentProcessingIndex]);
+
+
+				
+				allBlobs.push(Fileblob)
+
+
+				console.log(i)
+				console.log(currentProcessingIndex)
+				currentProcessingIndex++;
+			}
+		}
+
+
+			BlobToLinkCreator(allBlobs);
+
+			BlobToFileCreator(allBlobs);
+
+  }
+
+  function updateOptionSelect(value) {
+    optionSelect = value;
+  }
+
+
+async function imageCompressorFunction(image) {
+
+	document.querySelector(".images").innerHTML = ``;
+	imageSize = imageSize ? imageSize : 1024;
+
+
+const imageFile = image;
+console.log('originalFile instanceof Blob', imageFile instanceof Blob); // true
+console.log(`originalFile size ${imageFile.size / 1024 / 1024} MB`);
+
+const options = {
+	maxSizeMB: imageSize/1024,
+	// maxWidthOrHeight: 1920,
+	useWebWorker: true,
+}
+try {
+	const compressedFile = await imageCompression(imageFile, options);
+	console.log('compressedFile instanceof Blob', compressedFile instanceof Blob); // true
+	console.log(`compressedFile size ${compressedFile.size / 1024 / 1024} MB`); // smaller than maxSizeMB
+
+return await compressedFile;
+
+} catch (error) {
+	console.log(error);
+}
+
+}
+
+
+function BlobToLinkCreator(blobArray){
+
+	blobArray.forEach(Fileblob=>{
+		
+		let myBlob = Fileblob;
+
+		// Create a link element
+		let downloadLink = document.createElement('a');
+
+		// Create a URL for the Blob
+		let blobUrl = URL.createObjectURL(myBlob);
+
+		// Set the href attribute of the link to the Blob URL
+		downloadLink.href = blobUrl;
+
+		// Set the download attribute with the desired file name
+		downloadLink.download = myBlob.name // You can set any desired file name with the appropriate file extension
+
+		downloadLink.innerText = `download - ${myBlob.name}`
+
+		document.querySelector(".images").appendChild(downloadLink);
+	})
+
+}
+
+function BlobToFileCreator(blobArray){
+
+	let imageArray = [];
+	blobArray.forEach(myBlob=>{
+		let imageFile = new File([myBlob],myBlob.name, { type: myBlob.type });
+
+		imageArray.push(imageFile);
+	})
+
+	console.log(imageArray)
+}
+</script>
+
+<main class="py-5">
+  <div class="h-full w-full grid grid-cols-1 gap-5 justify-items-center">
+    <input type="file" name="" class="border rounded-full cursor-pointer" multiple bind:files={files}>
+    <div class="space-y-2">
+      <label class="flex items-center space-x-2">
+        <!-- <input class="radio" type="radio" name="radio-direct" value="Size" bind:group={optionSelect} on:change={() => updateOptionSelect("Size")} /> -->
+        <input type="number" placeholder="FileSize" class="rounded-full disabled:variant-filled-surface px-3 py-1 w-24 text-slate-900" disabled="{optionSelect !== 'Size'}" bind:value={imageSize}> 
+        <span>KB</span>
+      </label>
+      <!-- <label class="flex items-center space-x-2">
+        <input class="radio" type="radio" name="radio-direct" value="heightWidth" bind:group={optionSelect} on:change={() => updateOptionSelect("heightWidth")} />
+        <div class="flex gap-2 items-center">
+          <input type="number" name="" id="" class="rounded-full disabled:variant-filled-surface px-3 py-1 w-28 text-slate-900" placeholder="Height in px" disabled="{optionSelect !== 'heightWidth'}" bind:value={imageHeight}>
+          <span>X</span>
+          <input type="number" name="" id="" class="rounded-full disabled:variant-filled-surface px-3 py-1 w-28 text-slate-900" placeholder="Width in px" disabled="{optionSelect !== 'heightWidth'}" bind:value={imageWidth}>
+        </div>
+      </label> -->
+    </div>
+    <div>
+      <button type="button" class="btn variant-glass-primary" on:click={() => compress_image()}>Compress</button>
+    </div>
+  </div>
+
+	<div class="ps-5">
+		<p>number of images processed : {currentProcessingIndex}</p>
+		<ProgressRadial value={((currentProcessingIndex/files.length)*100).toFixed(2)} stroke={100} meter="stroke-primary-500" track="stroke-primary-500/30" strokeLinecap="round">{((currentProcessingIndex/files.length)*100).toFixed(2)}%</ProgressRadial>
+	</div>
+
+	<div class="images flex flex-col w-50 items-center mt-12">
+
+	</div>
+
+
+</main>
+
+<style>
+  input::-webkit-outer-spin-button,
+  input::-webkit-inner-spin-button {
+    -webkit-appearance: none;
+    margin: 0;
+  }
+
+  input[type=number] {
+    -moz-appearance: textfield;
+		appearance: textfield;
+  }
+</style>
